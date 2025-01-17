be913a99848037280ffc3fbf0145ca96a1193e59
ptt98hd <thanhpham.hd.98@gmail.com>
2025-01-17 22:54:06 +0700

    basic Routes management

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,18 @@
-import {Routes, Route} from "react-router";
+import { Routes, Route } from 'react-router';
+import Login from './Login';
+import Test from './Test';
 
-function Page() {
-    return (
-        <div className='container-fluid container-lg my-5'>
-            <Routes>
-                <Route path="/" element={<h1>Home</h1>}/>
-                <Route path="/login" element={<h1>LogIn</h1>}/>
-                <Route path="/signup" element={<h1>SignUp</h1>}/>
-            </Routes>
-        </div>
-    );
+function Pages() {
+	return (
+		<div className='container-fluid container-lg my-5'>
+			<Routes>
+				<Route path='/' element={<h1>Home</h1>} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/signup' element={<h1>SignUp</h1>} />
+				<Route path='/test' element={<Test />} />
+			</Routes>
+		</div>
+	);
 }
 
-export default Page;
+export default Pages;
