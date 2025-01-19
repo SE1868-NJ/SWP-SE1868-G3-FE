f1487968ccd3bf7c0f4faa6b1b4d7af50e5b9745
ptt98hd <thanhpham.hd.98@gmail.com>
2025-01-19 19:03:35 +0700

    optimize component usage and className

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,85 +1,90 @@
 import { Link } from 'react-router';
 import { Row, Col } from '../components/Grid';
+import Stack from '../components/Stack';
+import Card from '../components/Card';
 import loginImg from '../assets/images/login.png';
+import Container from '../components/Container';
 
 function Login() {
 	return (
 		<Row cols={1} md={2} className='g-0 g-md-5'>
 			<Col md={6} lg={7} className='d-none d-md-flex'>
-				<div className='container my-auto'>
+				<Container className='my-auto'>
 					<h2 className='text-danger fw-bold'>Bán hàng chuyên nghiệp</h2>
 					<p className='h4 mb-5'>
 						Quản lý shop của bạn hiệu quả hơn với Kênh người bán hàng
 					</p>
 					<img src={loginImg} alt='Đăng nhập' className='img-fluid' />
-				</div>
+				</Container>
 			</Col>
 			<Col md={6} lg={5}>
-				<div className='card w-100 shadow-sm'>
-					<div className='card-header'>
+				<Card className='shadow-sm'>
+					<Card.Header>
 						<h1 className='text-center my-3'>Đăng nhập</h1>
-					</div>
-					<div className='card-body vstack gap-4 my-3'>
-						<div className='input-group'>
-							<i className='bi bi-envelope input-group-text'></i>
-							<input
-								type='email'
-								className='form-control'
-								placeholder='Email'
-							/>
-						</div>
-						<div className='input-group'>
-							<i className='bi bi-shield-lock input-group-text' />
-							<input
-								type='password'
-								className='form-control'
-								placeholder='Password'
-							/>
-						</div>
-						<div className='w-100 d-flex flex-column gap-2'>
-							<button className='btn btn-lg btn-success w-100 fw-bold'>
-								LogIn
-							</button>
-							<Link
-								to='/forgot-password'
-								className='fw-bold text-center text-decoration-none'
+					</Card.Header>
+					<Card.Body className='my-3'>
+						<Stack direction={'v'} gap={3}>
+							<div className='input-group'>
+								<i className='bi bi-envelope input-group-text'></i>
+								<input
+									type='email'
+									className='form-control'
+									placeholder='Email'
+								/>
+							</div>
+							<div className='input-group'>
+								<i className='bi bi-shield-lock input-group-text' />
+								<input
+									type='password'
+									className='form-control'
+									placeholder='Password'
+								/>
+							</div>
+							<div className='w-100 d-flex flex-column gap-2'>
+								<button className='btn btn-lg btn-success w-100 fw-bold'>
+									LogIn
+								</button>
+								<Link
+									to='/forgot-password'
+									className='fw-bold text-center text-decoration-none'
+								>
+									Quên mật khẩu?
+								</Link>
+							</div>
+							<div
+								className='position-relative d-flex'
+								style={{ height: '1rem' }}
 							>
-								Quên mật khẩu?
-							</Link>
-						</div>
-						<div
-							className='position-relative d-flex'
-							style={{ height: '1rem' }}
-						>
-							<hr className='my-auto w-100' />
-							<div className='position-absolute start-50 top-50 translate-middle text-center px-3 bg-white fw-bold'>
-								HOẶC
+								<hr className='my-auto w-100' />
+								<div className='position-absolute start-50 top-50 translate-middle text-center px-3 bg-white fw-bold'>
+									HOẶC
+								</div>
 							</div>
-						</div>
-						<div className='row row-cols-2'>
-							<div className='col'>
-								<Link className='btn btn-light w-100 border fw-bold text-bg-danger'>
-									<i className='bi bi-google me-2 d-none d-sm-inline-block'></i>
-									<span>Google</span>
-								</Link>
+							<div className='row row-cols-2'>
+								<div className='col'>
+									<Link className='btn btn-light w-100 border fw-bold text-bg-danger'>
+										<i className='bi bi-google me-2 d-none d-sm-inline-block'></i>
+										<span>Google</span>
+									</Link>
+								</div>
+								<div className='col'>
+									<Link className='btn btn-light w-100 border fw-bold text-bg-primary'>
+										<i className='bi bi-facebook me-2 d-none d-sm-inline-block' />
+										<span>Facebook</span>
+									</Link>
+								</div>
 							</div>
-							<div className='col'>
-								<Link className='btn btn-light w-100 border fw-bold text-bg-primary'>
-									<i className='bi bi-facebook me-2 d-none d-sm-inline-block' />
-									<span>Facebook</span>
-								</Link>
-							</div>
-						</div>
-					</div>
-					<div className='card-footer'>
+						</Stack>
+					</Card.Body>
+					<Card.Footer>
 						<div className='text-center'>
 							<span className='fw-bold'>Bạn mới đến Chợ Làng?</span>{' '}
 							<Link to='/signup' className='fw-bold text-danger'>
 								Đăng ký
 							</Link>
 						</div>
-					</div>
-				</div>
+					</Card.Footer>
+				</Card>
 			</Col>
 		</Row>
 	);
