906eabfc66832f0f3c437a4fe2b9286b54dda561
NhanPhan <ptn987654321@gmail.com>
2025-02-17 18:43:31 +0700

    Update resolve conflict in ProductCard.jsx

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -7,12 +7,17 @@ const ProductCard = ({ product, onZoom, onAddToCart, user_id }) => {
   const navigate = useNavigate();
 
   const handleNavigate = () => {
-    navigate(`/product/${product.id}`);
+    navigate(`/product/${product.id || product._id || product.product_id}`);
   };
 
   return (
     <Card className="position-relative">
-      <Card.Img variant="top" src={product.image_url || product.img} style={{ cursor: "pointer" }} onClick={handleNavigate} />
+      <Card.Img
+        variant="top"
+        src={product.image_url || product.img}
+        style={{ cursor: "pointer" }}
+        onClick={handleNavigate}
+      />
       <Card.Footer>
         <div
           className="position-absolute top-0 end-0 p-2 bg-danger text-white rounded-circle"
@@ -26,19 +31,19 @@ const ProductCard = ({ product, onZoom, onAddToCart, user_id }) => {
         <Card.Title onClick={handleNavigate} style={{ cursor: "pointer" }}>
           {product.product_name || product.name}
         </Card.Title>
-        <Card.Text>{product.sale_price || product.price} VND</Card.Text>
+        <Card.Text>{product.sale_price?.toLocaleString() || product.price?.toLocaleString()} VND</Card.Text>
         <div className="d-flex justify-content-between">
           <Button variant="danger" size="sm">
             Mua Ngay
           </Button>
           <Button variant="outline-danger" size="sm"
-            onClick={() => onAddToCart(product.id, user_id)}
+            onClick={() => onAddToCart(product.id || product._id || product.product_id, user_id)}
           >
             Thêm vào giỏ hàng
           </Button>
         </div>
-      </Card.Body >
-    </Card >
+      </Card.Body>
+    </Card>
   );
 };
 
