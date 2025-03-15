03f6f692b54c491dc5ffec865d71b339528d7545
NhanPhan <ptn987654321@gmail.com>
2025-03-16 00:17:13 +0700

    Update layout

--- src/layout/ShopLayout/index.jsx
+++ src/layout/ShopLayout/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
-import Footer from "./Footer.jsx";
-import Header from "./Header.jsx";
+import Footer from "../Footer.jsx";
+import Header from "../Header.jsx";
 import ChatPopup from "../../components/Chats/ChatPopup.jsx";
 import { ChatProvider } from "../../hooks/contexts/ChatContext.jsx";
 import { useAuth } from "../../hooks/contexts/AuthContext.jsx";
